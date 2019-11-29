bd9f7c79243b7628961d22c4a558750c9552c49f
MohamedSaeed99 <msaeed7@uic.edu>
2019-11-28 19:02:28 -0600

    Added service worker along with manifest

---
+++ sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'app-v1';
+var appShellFiles = [
+    "./index.html",
+    "./styles.css"
+];
+
+
+//installs all the files as back up
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+ e.request.url);
+      
+      return r || fetch(e.request).then(function(response) {
+          return caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+        });
+      });
+    })
+  );
+});
